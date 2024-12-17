6da70de591b80d837670ac4955ec48e4b63a3ae5
Rasoul <smrasoul76@gmail.com>
2024-12-17 14:42:23 +0330

    firstName, lastName and email added according to old error handling.

--- includes/signup-functions.php
+++ includes/signup-functions.php
@@ -1,9 +1,21 @@
 <?php
 
 
-function validateSignup($username, $password) {
+function validateSignup($firstName, $lastName, $email, $username, $password) {
 
     $errors = array();
+
+    if($firstName == ''){
+        $errors[] = "First Name is required";
+    }
+
+    if($lastName == ''){
+        $errors[] = "Last Name is required";
+    }
+
+    if($email == ''){
+        $errors[] = "Email is required";
+    }
 
     if($username == ''){
         $errors['username_error'] = "username is empty";
@@ -12,6 +24,29 @@ function validateSignup($username, $password) {
         $errors['password_error'] = "password is empty";
     }
     return $errors;
+}
+
+function checkEmailAvailability($email, $conn) {
+
+    $errors = array();
+
+    $query = "SELECT * 
+                  FROM users
+                  WHERE email = ?";
+
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, 's', $email);  // 's' indicates the string parameter
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
+    $email_check = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+    if ($email_check && $email_check['email'] === $email){
+        $errors['email_availability_error'] = "Email is already taken.";
+    };
+
+    return $errors;
+
 }
 
 function checkUsernameAvailability($username, $conn) {
--- signup.php
+++ signup.php
@@ -4,15 +4,25 @@ require 'includes/init.php';
 require 'includes/url.php';
 require 'includes/signup-functions.php';
 
+$firstName = '';
+$lastName = '';
+$email = '';
 $username = '';
 $password = '';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+    $firstName = htmlspecialchars($_POST['firstName']);
+    $lastName = htmlspecialchars($_POST['lastName']);
+    $email = htmlspecialchars($_POST['email']);
     $username = htmlspecialchars($_POST['username']);
     $password = htmlspecialchars($_POST['password']);
 
-    $errors = validateSignup($username, $password);
+    $errors = validateSignup($firstName, $lastName, $email, $username, $password);
+
+    if(empty($errors)) {
+        $errors = checkEmailAvailability($email, $conn);
+    }
 
     if (empty($errors)) {
         $errors = checkUsernameAvailability($username, $conn);
@@ -44,15 +54,33 @@ require 'includes/header.php';
     <?php endif; ?>
     <form method="POST">
         <div class="form-group mb-2 row">
+            <label for="firstName">First name</label>
+            <div class="col-4">
+                <input type="text" class="form-control" id="firstName" name="firstName" value="<?= $firstName ?>">
+            </div>
+        </div>
+        <div class="form-group mb-2 row">
+            <label for="lastName">Last name</label>
+            <div class="col-4">
+                <input type="text" class="form-control" id="lastName" name="lastName" value="<?= $lastName ?>">
+            </div>
+        </div>
+        <div class="form-group mb-2 row">
+            <label for="email">Email</label>
+            <div class="col-4">
+                <input type="email" class="form-control" id="email" name="email" value="<?= $email ?>">
+            </div>
+        </div>
+        <div class="form-group mb-2 row">
             <label for="username">Username</label>
             <div class="col-4">
-                <input type="text" class="form-control" id="username" name="username" value="<?= $username ?>" required>
+                <input type="text" class="form-control" id="username" name="username" value="<?= $username ?>">
             </div>
         </div>
         <div class="form-group mb-2 row">
             <label for="password">Password</label>
             <div class="col-4">
-                <input type="password" class="form-control" id="password" name="password" value="<?= $password ?>" required>
+                <input type="password" class="form-control" id="password" name="password" value="<?= $password ?>">
             </div>
         </div>
         <button type="submit" class="btn btn-primary">Sign Up</button>
